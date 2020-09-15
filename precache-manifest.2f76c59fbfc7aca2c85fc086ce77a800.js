f9fd1db0376c024ddd40823985df9ee2f3d4c300
jbresolinn <juliabresolin2@gmail.com>
2020-09-15 14:19:00 -0300

    Updates

---
+++ precache-manifest.2f76c59fbfc7aca2c85fc086ce77a800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f24ef468d318ac93cff7c1395cf797",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "aa7191a0339ef3317246",
+    "url": "/github-explorer/static/js/2.f4e826e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.f4e826e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84b64ea5be026736abe",
+    "url": "/github-explorer/static/js/main.4af61211.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
